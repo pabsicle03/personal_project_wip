18df23fdb6a762120c2083fb413d47739ed6a336
Pablo <pablo.gonzalez0328@gmail.com>
2026-04-13 19:32:24 -0500

    Initial upload

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,42 +1,22 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
+// We use process.env to grab the keys from your .env file
 const firebaseConfig = {
-  apiKey: "AIzaSyAcaR36JZk_Fw6JVGK_ZBWnJIbzlRkx9GM",
-  authDomain: "roommatematch-95763.firebaseapp.com",
-  projectId: "roommatematch-95763",
-  storageBucket: "roommatematch-95763.firebasestorage.app",
-  messagingSenderId: "761114797653",
-  appId: "1:761114797653:web:b94b84f5392338d112d6dc"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
-
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 
-export const signUpUser = async (email, password) => {
-  // 1. Strict .edu check
-  if (!email.endsWith('.edu')) {
-    throw new Error("Please use your official college (.edu) email address.");
-  }
-
-  try {
-    // 2. Create the account
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-
-    // 3. Send verification email
-    // This ensures they actually own the college inbox
-    await sendEmailVerification(user);
-    
-    return "Verification email sent! Please check your inbox.";
-  } catch (error) {
-    throw new Error(error.message);
-  }
-};
+export default app;
